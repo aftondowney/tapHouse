a84d890b192ed5aac388030b40b2d715d8093508
Afton Downey <aaike001@gmail.com>
2016-03-23 19:29:51 -0700

    add keg-list.component

--- app/keg.model.ts
+++ app/keg.model.ts
@@ -1,4 +1,7 @@
+
 export class Keg {
   public empty: boolean = false;
-  constructor(public brewery: string, public name: string, public type: string, public abv: number, public price: number, public id: number)
+  constructor(public brewery: string, public name: string, public type: string, public abv: number, public price: number, public id: number) {
+    
+  }
 }
